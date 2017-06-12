e2eec8c6a3ccec694bf781cb9c6c2d3b63f813cb
AgesilausDrako <agesilaus@yahoo.com>
2017-06-11 20:26:04 -0400

    cleaned up spacing and added explanation comments to js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,20 @@
+//Establish the computer's choices in a variable
 var computerChoices = ['a','b','c','d','e','f','g','h','i','j','k','l',
 'm','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-
+//Global variables for wins, losses, guesses, and the guess storage array
 var wins = 0;
 var losses = 0;
 var guesses = 9;
 var guessArr = [];
 
-
+//Function fired my keyup event
 document.onkeyup = function(event) { 
 
 var userGuess = event.key;
+//Computer's random guess
 var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-	
+	//Array for guess storage 
 	guessArr.push(userGuess);
 
 	if (userGuess === computerGuess) {
@@ -22,16 +24,18 @@ var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.l
 		losses++;
 		guesses--;
 	}
-	
+	//HTML intended to update the site after each guess
 	var html = "<p>Guess what letter I'm thinking of</p>" +
           "<p>Wins: " + wins + "</p>" +
           "<p>Losses: " + losses + "</p>" +
           "<p>Guesses: " + guesses + "</p>"+
           "<p>Your guesses so far: " + guessArr.toString()  + "</p>";
 
-	// Injecting the HTML we just created into our div and updating the game information on our page.
+	//Query selector updates the div with the HTML above 
 	document.querySelector("#game").innerHTML = html;
 
+	/*Reset which changes wins, losses, and guesses to 0 after 
+	9 attempts and clears the array*/
 	if (wins === 9 || losses === 9 || guesses === 0) {
 		wins=0;
 		losses=0;
@@ -45,7 +49,6 @@ var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.l
  
 	document.querySelector("#game").innerHTML = resetHtml;
 	}
-
 }
 
 
